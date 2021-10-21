1708e9df5de86177e41ac723c9748adb6119e2d5
Jeronimo Rodriguez Gonzalez <jero-rodriguez@users.noreply.github.com>
2021-10-21 23:04:12 +0200

    skeleton

---
+++ src/main/java/com/cubetek/dmtools/DmtoolsApiApplication.java
@@ -0,0 +1,13 @@
+package com.cubetek.dmtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmtoolsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmtoolsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cubetek/dmtools/DmtoolsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cubetek.dmtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmtoolsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
